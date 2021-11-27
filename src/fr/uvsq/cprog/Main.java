06b1c56f259d711c7c3acf8ea8c79a6c7b64d7f6
Yasser El Habib DRIF <gy_drif@esi.dz>
2021-11-27 03:32:47 +0100

    s

--- src/fr/uvsq/cprog/Main.java
+++ src/fr/uvsq/cprog/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Fraction fraction = new Fraction(2,3);
-		System.out.println("La fraction en String "+fraction.toString());
+		System.out.println("La fraction en String"+fraction.toString());
 		
 	}
 
